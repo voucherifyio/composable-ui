ab40080dd957c874071a7888119dd03e3f851b55
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-11-15 13:34:16 +0100

    Fix localStorage name

--- packages/voucherify/data/persit.ts
+++ packages/voucherify/data/persit.ts
@@ -7,9 +7,9 @@ const storageFolderPath = path.join(
   'composable-ui-storage-voucherify'
 )
 
-const localStarege = storage.create()
+const localStorage = storage.create()
 
-localStarege.init({
+localStorage.init({
   dir: storageFolderPath,
 })
 
@@ -18,18 +18,18 @@ console.log(
 )
 
 export const getCartDiscounts = async (cartId: string): Promise<string[]> => {
-  return (await localStarege.getItem(`cart-discounts-${cartId}`)) || []
+  return (await localStorage.getItem(`cart-discounts-${cartId}`)) || []
 }
 
 export const saveCartDiscounts = async (
   cartId: string,
   discounts: string[]
 ) => {
-  await localStarege.setItem(`cart-discounts-${cartId}`, discounts)
+  await localStorage.setItem(`cart-discounts-${cartId}`, discounts)
   return discounts
 }
 
 export const deleteCartDiscounts = async (cartId: string) => {
-  const result = await localStarege.del(`cart-discounts-${cartId}`)
+  const result = await localStorage.del(`cart-discounts-${cartId}`)
   return result.removed
 }
