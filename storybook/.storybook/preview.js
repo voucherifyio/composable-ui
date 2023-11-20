33604bcd0e761701f558c34c7cbf4d964b0a6e87
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-11-20 21:03:56 +0100

    Add intlDecorator for viewing in Storybook

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,4 +1,7 @@
 import { theme } from '@composable/ui'
+import { addDecorator } from '@storybook/react'
+import { IntlProvider } from 'react-intl'
+import { intlConfig } from '../../composable-ui/src/server/intl/index'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,3 +15,17 @@ export const parameters = {
     theme,
   },
 }
+const intlDecorator = (Story, context) => {
+  const locale = context.globals?.locale || 'en-US'
+  const storyConfig = intlConfig.find((config) => config.locale === locale)
+  if (!storyConfig) {
+    console.error(`Missing intl config for locale: ${locale}`)
+  }
+  return (
+    <IntlProvider locale={locale} messages={storyConfig.keys}>
+      <Story />
+    </IntlProvider>
+  )
+}
+
+addDecorator(intlDecorator)
