c89261435429a6423756fcdb7482cd037335e263
weronika-kurczyna <117282008+weronika-kurczyna@users.noreply.github.com>
2023-11-20 21:32:16 +0100

    Add story for cart promotions

--- storybook/stories/CartPromotions.stories.tsx
+++ storybook/stories/CartPromotions.stories.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { CartPromotions } from '@composable/ui'
-import type { Meta } from '@storybook/react'
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Components/CartPromotions',
   component: CartPromotions,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {},
 } as ComponentMeta<typeof CartPromotions>
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof CartPromotions> = (args) => (
   <CartPromotions {...args} />
 )
 
 export const Default = Template.bind({})
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 Default.args = {
-  promotions: [],
+  promotions: [
+    {
+      id: 'id',
+      status: 'APPLICABLE',
+      object: 'promotion_tier',
+      label: '5$ OFF',
+      discount: '5',
+    },
+    {
+      id: 'id',
+      status: 'APPLICABLE',
+      object: 'promotion_tier',
+      label: '10$ OFF',
+      discount: '15',
+    },
+  ],
 }
